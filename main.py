a29c4a7f57e92eaa9cd548aec4926fe7da2e84c1
Artur Bartz <arturbylly2@gmail.com>
2025-05-02 09:41:14 -0300

    feat: implementa opção exit na main.py

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ from atm import ATM
 
 def main():
     print("Starting ATM System...")
-    atm = ATM()
+    try:
+        atm = ATM()
+    except KeyboardInterrupt:
+        print("\nATM session terminated by user.")
+    except Exception as e:
+        print(f"\nError: {e}. ATM shutting down.")
+    finally:
+        print("Thank you for using our ATM. Goodbye!")
 
 if __name__ == "__main__":
     main()
