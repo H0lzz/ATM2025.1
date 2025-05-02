436ca1f1491d9f5dbac4beae083f4d35574df03d
Artur Bartz <arturbylly2@gmail.com>
2025-05-02 09:31:38 -0300

    feat: Adiciona o account_factory.py para criação de objetos account

---
+++ account_factory.py
@@ -0,0 +1,11 @@
+from account import Account, SavingsAccount, CheckingAccount
+
+class AccountFactory:
+    @staticmethod
+    def create_account(account_type, account_number, pin, available_balance=0, total_balance=0, is_admin=False):
+        if account_type == "savings":
+            return SavingsAccount(account_number, pin, available_balance, total_balance)
+        elif account_type == "checking":
+            return CheckingAccount(account_number, pin, available_balance, total_balance)
+        else:
+            return Account(account_number, pin, available_balance, total_balance, is_admin)
